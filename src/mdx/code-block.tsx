19c78535b638a3a5c75ca6897ac332378ce81efa
sleonia <nikita.dontsov@yahoo.com>
2022-04-30 19:43:45 +0300

    Add custon editor and change code

--- src/mdx/code-block.tsx
+++ src/mdx/code-block.tsx
@@ -8,46 +8,74 @@ import styled from '@emotion/styled'
 /* comment: alias for playground components */
 /* eslint-disable-next-line import/no-unresolved */
 import Playground from '@storybook21-aliases/playground'
-
+import { Button, Collapse, Paper, useMantineTheme } from '@mantine/core'
+import Editor, { OnValidate } from '@monaco-editor/react'
 import {
     LiveProvider,
-    LiveEditor,
+    // LiveEditor,
     LiveError,
+    LiveContext,
     LivePreview
+    // Editor
 } from 'react-live'
 
-// const LiveEditor = (props) => (
-//     <LiveContext.Consumer>
-//         {({ code, language, theme, disabled, onChange }) => {
-//             const handleChange = (liveCode) => {
-//                 if (props.onChange) {
-//                     props.onChange(liveCode)
-//                 }
-//                 onChange(liveCode)
-//             }
+const LiveEditor = ({ handleCode, ...rest }) => {
+console.log('🚀 ~ file: code-block.tsx ~ line 23 ~ LiveEditor ~ rest', rest)
+    const { colorScheme } = useMantineTheme()
 
-//             return (
-//                 <Editor
-//                     theme={theme}
-//                     code={code}
-//                     language={language}
-//                     disabled={disabled}
-//                     onChange={handleChange}
-//                     {...props}
-//                 />
-//             )
-//         }}
-//     </LiveContext.Consumer>
-// )
+    const handleEditorValidation: OnValidate = (markers) => {
+        markers.forEach((marker) => console.log('onValidate:', marker.message))
+    }
 
-export const CodeBlock = (props) => {
-console.log('🚀 ~ file: code-block.tsx ~ line 44 ~ CodeBlock ~ props', props)
+    const [opened, setOpen] = useState(false)
 
     return (
-        <LiveProvider code={props.children} scope={{ Playground }} noInline>
-            <LiveEditor />
-            <LiveError />
+        <LiveContext.Consumer>
+            {({ code, language, theme, disabled, error }) => {
+                console.log('🚀 ~ file: code-block.tsx ~ line 59 ~ LiveEditor ~ code, language, theme, disabled, error', code, language, theme, disabled, error)
+
+                return (
+                    <div style={{ marginTop: '20px' }}>
+                        <Button onClick={() => setOpen((o) => !o)}>
+                            Toggle content
+                        </Button>
+
+                        <Collapse in={opened}>
+                            <Paper shadow="xs" p="md" m="md">
+                                <Editor
+                                    height="200px"
+                                    defaultLanguage="javascript"
+                                    defaultValue={code || "// some comment"}
+                                    theme={colorScheme === 'light' ? 'vs' : 'vs-dark'}
+                                    onValidate={handleEditorValidation}
+                                    onChange={(value) => handleCode(value)}
+                                    options={{
+                                        minimap: {
+                                            enabled: false
+                                        }
+                                    }}
+                                />
+                            </Paper>
+                        </Collapse>
+                    </div>
+                )
+            }}
+
+        </LiveContext.Consumer>
+
+    )
+}
+
+export const CodeBlock = (props) => {
+    console.log('🚀 ~ file: code-block.tsx ~ line 44 ~ CodeBlock ~ props', props)
+
+    const [code, setCode] = useState(props.children)
+
+    return (
+        <LiveProvider code={code} scope={{ Playground }} noInline>
             <LivePreview />
+            <LiveEditor handleCode={(value) => setCode(value)} />
+            {/* <LiveError /> // TODO обработать ошибку */}
         </LiveProvider>
     )
 }
